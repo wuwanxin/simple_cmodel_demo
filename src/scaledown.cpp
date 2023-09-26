416105e889cbaaba42cc8ca14d068678cf998ee7
wuwanxin <896678777@qq.com>
2023-09-26 13:35:53 +0800

    a comment for the scaledown funciton for scaledown_row

--- src/scaledown.cpp
+++ src/scaledown.cpp
@@ -11,6 +11,23 @@ void scaledown(top_register top_reg, scaledown_register scaledown_reg, stream_u1
     uint10 u_linebuffer[3][8192];
     uint10 v_linebuffer[3][8192];
 
+/**
+
+    scaledown_row在HLS代码中的命名与tcl脚本中的set_directive_loop_tripcount指令是有关系的。
+
+    在Vitis HLS中，set_directive_loop_tripcount指令用于指定循环迭代次数的上限和平均值。它可以帮助优化器更好地对循环进行优化，并生成更高效的硬件设计。
+
+    在示例中，set_directive_loop_tripcount指令中设置了两个参数：
+
+    -max 8192：指定循环迭代次数的最大值为8192。
+    -avg 1920：指定循环迭代次数的平均值为1920。
+    这些参数告诉优化器，在考虑性能和资源使用的情况下，循环迭代次数应尽可能接近这些值。
+
+    而在HLS代码中，scaledown_row被用作标识一个循环或代码块，该循环或代码块的迭代次数将受到tcl脚本中set_directive_loop_tripcount指令的影响。通过这种方式，你可以将特定的循环或代码块与指定的循环迭代次数约束相关联。
+
+    总结起来，scaledown_row在HLS代码中的命名与tcl脚本中的set_directive_loop_tripcount指令是配合使用的，用于指定循环迭代次数的约束和优化。这种方式可以帮助你更好地控制硬件设计的性能和资源利用情况。
+    
+*/
     scaledown_row: for (uint13 y = 0; y < top_reg.frameHeight; y++) {
         scaledown_col: for (uint13 x = 0; x < top_reg.frameWidth; x++) {
 
